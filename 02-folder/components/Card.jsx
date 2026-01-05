ba66b2924487df0dfd388c7fc1cf36feb7fb1864
ayusharyan4269-bit <ayusharyan4269@gmail.com>
2026-01-05 23:07:42 +0530

    this is new 2nd folder

---
+++ 02-folder/components/Card.jsx
@@ -0,0 +1,12 @@
+function Card(){
+  const user = 'AYUSH'
+  const age = 18
+    return (
+    
+    <div className='card'>
+  <h3>I am {user} and of {age} years </h3>
+
+</div>
+    )
+}
+export default Card
